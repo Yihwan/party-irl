12142748ee87076d091772c7ed7f7aff6addc9d0
yihwan <hi@yihwan.kim>
2022-04-02 14:46:07 -0700

    add backend

--- programs/party-irl/src/lib.rs
+++ programs/party-irl/src/lib.rs
@@ -6,10 +6,271 @@ declare_id!("XxFzjuDV8dptxFvY5v4AzQZk9j8h8Afpoi8WNU6uVtq");
 pub mod party_irl {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_party(
+        ctx: Context<CreateParty>,
+        name: String,
+        maximum_guests: u32,
+        party_at: i64,
+        check_in_ends_at: i64,
+        stake_in_lamports: u64,
+    ) -> Result<()> {
+        let party: &mut Account<Party> = &mut ctx.accounts.party;
+        let creator: &Signer = &ctx.accounts.creator;
+        let clock: Clock = Clock::get().unwrap();
+
+        // Validations 
+        if name.chars().count() > 64 {
+            return Err(ErrorCode::PartyNameTooLong.into())
+        }
+        if party_at < clock.unix_timestamp {
+            return Err(ErrorCode::PartyAtInThePast.into())
+        }
+        if check_in_ends_at - party_at < 5 * 60 {
+            return Err(ErrorCode::CheckInTimeTooShort.into())
+        }
+
+        party.creator = *creator.key;
+        party.name = name;
+        party.party_at = party_at;
+        party.check_in_ends_at = check_in_ends_at;
+        party.stake_in_lamports = stake_in_lamports;
+        party.maximum_guests = maximum_guests;
+
+        party.added_guests_count = 0;
+        party.checked_in_guests_count = 0;
+
+        Ok(())
+    }
+
+    pub fn add_guest(
+        ctx: Context<AddGuest>,
+    ) -> Result<()> {
+        let guest: &mut Account<Guest> = &mut ctx.accounts.guest;
+        let party: &mut Account<Party> = &mut ctx.accounts.party;
+        let guest_authority: &Signer = &ctx.accounts.guest_authority;
+        let clock: Clock = Clock::get().unwrap();
+
+        if party.check_in_ends_at < clock.unix_timestamp {
+            return Err(ErrorCode::CheckInNotOpen.into())
+        }
+        if party.added_guests_count >= party.maximum_guests {
+            return Err(ErrorCode::PartyFull.into())
+        }
+
+        let add_guest_transfer = anchor_lang::solana_program::system_instruction::transfer(
+            &guest_authority.key(),
+            &party.key(),
+            party.stake_in_lamports,
+        );
+        anchor_lang::solana_program::program::invoke(
+            &add_guest_transfer,
+            &[
+                guest_authority.to_account_info(),
+                party.to_account_info(),
+            ],
+        )?;
+
+        guest.guest_authority = *guest_authority.key;
+        guest.party = party.key();
+        guest.has_checked_in = false;
+        guest.has_settled_stake = false;
+        guest.bump = *ctx.bumps.get("guest").unwrap();
+
+        party.added_guests_count += 1;
+
+        Ok(())
+    }
+
+    pub fn check_in_guest(
+        ctx: Context<CheckInGuest>,
+    ) -> Result<()> {
+        let guest: &mut Account<Guest> = &mut ctx.accounts.guest;
+        let party: &mut Account<Party> = &mut ctx.accounts.party;
+        let clock: Clock = Clock::get().unwrap();
+
+        if party.party_at > clock.unix_timestamp {
+            return Err(ErrorCode::CheckInNotOpen.into())
+        }
+        if party.check_in_ends_at < clock.unix_timestamp {
+            return Err(ErrorCode::CheckInNotOpen.into())
+        }
+        if guest.has_checked_in {
+            return Err(ErrorCode::AlreadyCheckedIn.into())
+        }
+
+        guest.has_checked_in = true;
+
+        party.checked_in_guests_count += 1;
+
+        Ok(())
+    }
+
+    pub fn settle_guest(
+        ctx: Context<SettleGuest>,
+    ) -> Result<()> {
+        let guest: &mut Account<Guest> = &mut ctx.accounts.guest;
+        let party: &mut Account<Party> = &mut ctx.accounts.party;
+        let guest_authority: &Signer = &ctx.accounts.guest_authority;
+        let clock: Clock = Clock::get().unwrap();
+
+        if party.check_in_ends_at > clock.unix_timestamp {
+            return Err(ErrorCode::TooEarlyToSettle.into())
+        }
+        if !guest.has_checked_in || guest.has_settled_stake {
+            return Err(ErrorCode::CannotSettle.into())
+        }
+
+        let settle_amount = (party.stake_in_lamports * party.added_guests_count as u64) / party.checked_in_guests_count as u64;
+
+        **party.to_account_info().lamports.borrow_mut() -= settle_amount;
+        **guest_authority.lamports.borrow_mut() += settle_amount;
+
+        guest.has_settled_stake = true;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(name: String)]
+pub struct CreateParty<'info> {
+    #[account(
+        init, 
+        payer = creator, 
+        space = Party::space(&name),
+    )]
+    pub party: Account<'info, Party>,
+    #[account(mut)]
+    pub creator: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGuest<'info> {
+    #[account(
+        init, 
+        payer = guest_authority, 
+        space = Guest::LENGTH,
+        seeds=[
+            b"guest",
+            party.to_account_info().key().as_ref(),
+            guest_authority.to_account_info().key().as_ref(),
+        ],
+        bump
+    )]
+    pub guest: Account<'info, Guest>,
+    #[account(mut)]
+    pub party: Account<'info, Party>,
+    #[account(mut)]
+    pub guest_authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CheckInGuest<'info> {
+    #[account(
+        mut, 
+        has_one = guest_authority,
+        seeds=[
+            b"guest",
+            party.to_account_info().key().as_ref(),
+            guest_authority.to_account_info().key().as_ref(),
+        ],
+        bump=guest.bump
+    )]
+    pub guest: Account<'info, Guest>,
+    #[account(mut)]
+    pub party: Account<'info, Party>,
+    pub guest_authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SettleGuest<'info> {
+    #[account(
+        mut,
+        has_one = guest_authority,
+        seeds=[
+            b"guest",
+            party.to_account_info().key().as_ref(),
+            guest_authority.to_account_info().key().as_ref(),
+        ],
+        bump=guest.bump
+    )]
+    pub guest: Account<'info, Guest>,
+    #[account(mut)]
+    pub party: Account<'info, Party>,
+    #[account(mut)]
+    pub guest_authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Party {
+    pub creator: Pubkey,
+    pub name: String, // max 64 characters
+    pub party_at: i64,
+    pub check_in_ends_at: i64,
+    pub stake_in_lamports: u64,
+    pub maximum_guests: u32,
+    pub added_guests_count: u32,
+    pub checked_in_guests_count: u32,
+}
+
+#[account]
+pub struct Guest {
+    pub bump: u8,
+    pub guest_authority: Pubkey,
+    pub party: Pubkey,
+    pub has_checked_in: bool,
+    pub has_settled_stake: bool,
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const STRING_LENGTH_PREFIX: usize = 4;
+const TIMESTAMP_LENGTH: usize = 8;
+const STAKE_IN_LAMPORTS_LENGTH: usize = 8;
+
+impl Party {
+    fn space(name: &str) -> usize {
+        DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // creator
+        + STRING_LENGTH_PREFIX + name.len() // name 
+        + TIMESTAMP_LENGTH // party_at
+        + TIMESTAMP_LENGTH // check_in_ends_at
+        + STAKE_IN_LAMPORTS_LENGTH // stake_in_lamports
+        + 4 // maximum_guests
+        + 4 // added_guests_count
+        + 4 // checked_in_guests_count
+    }
+}
+
+impl Guest {
+    const LENGTH: usize = DISCRIMINATOR_LENGTH 
+        + PUBLIC_KEY_LENGTH // party
+        + PUBLIC_KEY_LENGTH // guest
+        + 1 // has_checked_in
+        + 1 // has_settled_stake
+        + 1; // bump
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Party name can't be longer than 64 characters.")]
+    PartyNameTooLong,
+    #[msg("Party start time can't be in the past.")]
+    PartyAtInThePast,
+    #[msg("Party is full.")]
+    PartyFull,
+    #[msg("Check-in time must be longer than 5 minutes.")]
+    CheckInTimeTooShort,
+    #[msg("Check-in is not open.")]
+    CheckInNotOpen,
+    #[msg("You have already checked-in.")]
+    AlreadyCheckedIn,
+    #[msg("Check-in is still open.")]
+    TooEarlyToSettle,
+    #[msg("You cannot settle.")]
+    CannotSettle,
+}
--- tests/party-irl.ts
+++ tests/party-irl.ts
@@ -1,16 +1,305 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { assert, expect } from "chai";
 import { PartyIrl } from "../target/types/party_irl";
+import dayjs from 'dayjs';
+
+const { SystemProgram, LAMPORTS_PER_SOL } = anchor.web3;
+
+const DEFAULT_PARTY_NAME = 'Test Party Name';
+const DEFAULT_PARTY_AT = dayjs().add(24, 'hour').unix();
+const DEFAULT_CHECK_IN_ENDS_AT = dayjs().add(25, 'hour').unix();
+const DEFAULT_STAKE_IN_LAMPORTS = 1 * LAMPORTS_PER_SOL;
+const DEFAULT_MAXIMUM_GUESTS = 4;
 
 describe("party-irl", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.PartyIrl as Program<PartyIrl>;
+  const mainPublicKey = anchor.getProvider().wallet.publicKey;
+  const mainProgram = anchor.workspace.PartyIrl as Program<PartyIrl>;
+  
+  it('can create a party', async () => {
+    const party = await createParty();
+    const partyAccount = await mainProgram.account.party.fetch(party.publicKey);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+    assert.equal(partyAccount.name, DEFAULT_PARTY_NAME);
+    assert.equal(partyAccount.partyAt.toNumber(), DEFAULT_PARTY_AT);
+    assert.equal(partyAccount.checkInEndsAt.toNumber(), DEFAULT_CHECK_IN_ENDS_AT);
+    assert.equal(partyAccount.stakeInLamports.toNumber(), DEFAULT_STAKE_IN_LAMPORTS);
+    assert.equal(partyAccount.creator.toBase58(), mainPublicKey.toBase58());
+    assert.equal(partyAccount.addedGuestsCount, 0);
+    assert.equal(partyAccount.checkedInGuestsCount, 0);
   });
+
+  it('cannot create a party with a name longer than 64 chars', async () => {
+    try {
+      await createParty({ name: 'x'.repeat(65) });
+    } catch({ error }) {
+        assert.equal(error.errorCode.code, 'PartyNameTooLong');
+        return;
+    }
+
+    assert.fail('The instruction should have failed.');
+  });
+
+  it('cannot create a party that starts in the past', async () => {
+    try {
+      await createParty({ partyAt: 0 });
+    } catch({ error }) {
+        assert.equal(error.errorCode.code, 'PartyAtInThePast');
+        return;
+    }
+
+    assert.fail('The instruction should have failed.');
+  });
+
+  it('cannot create a party with a check-in time less than 5 min', async () => {
+    try {
+      await createParty({ 
+        partyAt: dayjs().add(60, 'min').unix(), 
+        checkInEndsAt: dayjs().add(61, 'min').unix(),
+      });
+    } catch({ error }) {
+        assert.equal(error.errorCode.code, 'CheckInTimeTooShort');
+        return;
+    }
+
+    assert.fail('The instruction should have failed.');
+  });
+
+  it('can add guest to an upcoming party', async () => {
+    const guest = await createUser();
+
+    const party = await createParty();
+    const initialPartyBalance = await getAccountBalance(party.publicKey);
+    
+    const guestPda = await addGuest({ party, guest });
+    const partyBalanceAfterAddingGuest = await getAccountBalance(party.publicKey);
+
+    const guestAccount = await mainProgram.account.guest.fetch(guestPda);
+    const partyAccount = await mainProgram.account.party.fetch(party.publicKey);
+
+    assert.equal(guestAccount.party.toBase58(), party.publicKey.toBase58());
+    assert.equal(guestAccount.guestAuthority.toBase58(), guest.key.publicKey.toBase58());
+    assert.isFalse(guestAccount.hasCheckedIn);
+    assert.isFalse(guestAccount.hasSettledStake);
+    assert.equal(partyAccount.addedGuestsCount, 1);
+  
+    assert.equal(partyBalanceAfterAddingGuest - initialPartyBalance, DEFAULT_STAKE_IN_LAMPORTS);
+  });
+
+  it('cannot add guest other than self', async () => {
+    try {
+      const guest = await createUser();
+      const rando = await createUser(); 
+
+      const party = await createParty();
+
+      const [guestAccount] = await anchor.web3.PublicKey.findProgramAddress(
+        ['guest', party.publicKey.toBytes(), guest.key.publicKey.toBytes()],
+        mainProgram.programId
+      );
+
+      const program = programForUser(guest);
+
+      await program.rpc.addGuest({
+        accounts: {
+          party: party.publicKey,
+          guest: guestAccount,
+          guestAuthority: rando.key.publicKey,
+          systemProgram: SystemProgram.programId,
+        }
+      });
+    } catch {
+      return;
+    }
+
+    assert.fail('The instruction should have failed.');
+  })
+
+  it('can check in a guest', async () => {
+    const guest = await createUser();
+    const party = await createParty({
+      partyAt: dayjs().unix(),
+      checkInEndsAt: dayjs().add(2, 'hour').unix(),
+    }); 
+    const guestPda = await addGuest({ party, guest }); 
+
+    const program = programForUser(guest);
+
+    await program.rpc.checkInGuest({
+      accounts: {
+        guest: guestPda,
+        party: party.publicKey,
+        guestAuthority: guest.key.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+    });
+
+    const guestAccount = await mainProgram.account.guest.fetch(guestPda);
+    const partyAccount = await mainProgram.account.party.fetch(party.publicKey);
+
+    assert.equal(guestAccount.hasCheckedIn, true);
+    assert.equal(partyAccount.addedGuestsCount, 1);
+    assert.equal(partyAccount.checkedInGuestsCount, 1);
+  });
+
+  it('cannot check in on behalf of another guest', async () => {
+    try {
+      const guest = await createUser();
+      const rando = await createUser();
+      const party = await createParty(); 
+      const guestPda = await addGuest({ party, guest }); 
+
+      const program = programForUser(guest);
+
+      await program.rpc.checkInGuest({
+        accounts: {
+          guest: guestPda,
+          party: party.publicKey,
+          guestAuthority: rando.key.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+      });
+
+      const guestAccount = await mainProgram.account.guest.fetch(guestPda);
+
+      assert.equal(guestAccount.hasCheckedIn, true);
+    } catch {
+      return;
+    }
+
+    assert.fail('The instruction should have failed.');
+  });
+
+  // TODO: Simulate system times, ran various settle scenarios manually for now.
+  // it('can settle guest', async () => {
+  //   const guest = await createUser();
+  //   const rando = await createUser();
+  //   const party = await createParty({
+  //     partyAt: dayjs().unix(),
+  //     checkInEndsAt: dayjs().add(10, 'minute').unix(),
+  //   }); 
+  //   const guestPda = await addGuest({ party, guest }); 
+
+  //   console.log('startGuestBalance', await getAccountBalance(guest.key.publicKey))
+  //   console.log('startPartyBalance', await getAccountBalance(party.publicKey))
+
+  //   const program = programForUser(guest);
+
+  //   await program.rpc.checkInGuest({
+  //     accounts: {
+  //       guest: guestPda,
+  //       party: party.publicKey,
+  //       guestAuthority: guest.key.publicKey,
+  //       systemProgram: anchor.web3.SystemProgram.programId,
+  //     },
+  //   });
+
+  //   await program.rpc.settleGuest({
+  //     accounts: {
+  //       guest: guestPda,
+  //       party: party.publicKey,
+  //       guestAuthority: guest.key.publicKey,
+  //       systemProgram: anchor.web3.SystemProgram.programId,
+  //     },
+  //   });
+  //   await program.rpc.settleGuest({
+  //     accounts: {
+  //       guest: guestPda,
+  //       party: party.publicKey,
+  //       guestAuthority: guest.key.publicKey,
+  //       systemProgram: anchor.web3.SystemProgram.programId,
+  //     },
+  //   });
+
+  //   const guestAccount = await mainProgram.account.guest.fetch(guestPda);
+  //   const partyAccount = await mainProgram.account.party.fetch(party.publicKey);
+
+  //   console.log('endPartyBalance', await getAccountBalance(party.publicKey))
+  //   console.log('endGuestBalance', await getAccountBalance(guest.key.publicKey))
+    
+  //   assert.equal(guestAccount.hasCheckedIn, true);
+  //   assert.equal(guestAccount.hasSettledStake, true);
+  //   assert.equal(partyAccount.addedGuestsCount, 1);
+  //   // assert.equal(partyAccount.checkedInGuestsCount, 1);
+  // });
+
+  const createParty = async ({
+    name = DEFAULT_PARTY_NAME,
+    maximumGuests = DEFAULT_MAXIMUM_GUESTS,
+    partyAt = DEFAULT_PARTY_AT,
+    checkInEndsAt = DEFAULT_CHECK_IN_ENDS_AT,
+    stakeInLamports = DEFAULT_STAKE_IN_LAMPORTS,
+  } = {}) => {
+    const party = anchor.web3.Keypair.generate();
+
+    await mainProgram.rpc.createParty(
+      name,
+      maximumGuests,
+      new anchor.BN(partyAt),
+      new anchor.BN(checkInEndsAt),
+      new anchor.BN(stakeInLamports),
+      {
+        accounts: {
+          party: party.publicKey,
+          creator: mainPublicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [party],
+      },
+    )
+
+    return party;
+  }
+
+  const addGuest = async ({ party, guest }) => {
+    const program = programForUser(guest);
+
+    const [guestPda] = await anchor.web3.PublicKey.findProgramAddress(
+      ['guest', party.publicKey.toBytes(), guest.key.publicKey.toBytes()],
+      program.programId
+    );
+
+    await program.rpc.addGuest({
+      accounts: {
+        party: party.publicKey,
+        guest: guestPda,
+        guestAuthority: guest.key.publicKey,
+        systemProgram: SystemProgram.programId,
+      }, 
+    });
+
+    return guestPda;
+  }
+
+  const programForUser = (user) => (
+    new anchor.Program(mainProgram.idl, mainProgram.programId, user.provider)
+  );
+
+  const createUser = async (airdropBalance = 10 * LAMPORTS_PER_SOL) =>{
+    let user = anchor.web3.Keypair.generate();
+    let sig = await provider.connection.requestAirdrop(user.publicKey, airdropBalance);
+    await provider.connection.confirmTransaction(sig);
+
+    let wallet = new anchor.Wallet(user);
+    let userProvider = new anchor.Provider(provider.connection, wallet, provider.opts);
+
+    return {
+      key: user,
+      wallet,
+      provider: userProvider,
+    };
+  }
+
+  async function getAccountBalance(pubkey) {
+    let account = await provider.connection.getAccountInfo(pubkey);
+    return account?.lamports ?? 0;
+  }
+
+  function expectBalance(actual, expected, message, slack = 20000) {
+    expect(actual, message).within(expected - slack, expected + slack);
+  }
 });
