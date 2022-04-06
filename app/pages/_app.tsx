ae9c7d7ec404e4cab5876665f14c697c31533df1
yihwan <hi@yihwan.kim>
2022-04-06 12:57:56 -0400

    do the stuff later

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,10 +1,19 @@
 import {ErrorBoundary} from 'react-error-boundary'
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 import WalletContext from 'src/contexts/wallet';
 import Layout from 'src/components/layout';
+
+const lightTheme = createTheme({
+  type: 'light',
+})
+
+const darkTheme = createTheme({
+  type: 'dark',
+})
 
 const ErrorFallback = ({ error }) => {
   return (
@@ -25,11 +34,20 @@ function MyApp({ Component, pageProps }: AppProps) {
         // reset the state of your app so the error doesn't happen again
       }}
     >
-      <NextUIProvider>
-        <WalletContext>
-          <Component {...pageProps} />
-        </WalletContext>
-      </NextUIProvider>
+      <NextThemesProvider
+        defaultTheme="system"
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className
+        }}
+      >
+        <NextUIProvider>
+          <WalletContext>
+            <Component {...pageProps} />
+          </WalletContext>
+        </NextUIProvider>
+      </NextThemesProvider>
     </ErrorBoundary>
   );
 }
--- app/src/components/layout/nav.tsx
+++ app/src/components/layout/nav.tsx
@@ -2,6 +2,8 @@ import { FC, useState, useEffect } from 'react';
 import { Container } from '@nextui-org/react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { useTheme as useNextTheme } from 'next-themes'
+import { useTheme } from '@nextui-org/react'
 
 const Nav: FC = () => {
   // from nextjs-breadcrumbs
@@ -10,7 +12,10 @@ const Nav: FC = () => {
   const [scrollPosition, setScrollPosition] = useState(
     (typeof window !== 'undefined' && window.pageYOffset) || 0
   );
-  console.log(breadcrumbs)
+
+  const { setTheme } = useNextTheme();
+  const { isDark } = useTheme();
+
   const hasScrolled = scrollPosition > 0;
 
   useEffect(() => {
@@ -37,39 +42,66 @@ const Nav: FC = () => {
   }, [router]);
   
   return (
-    <nav 
-      style={{ 
-        fontSize: '18px',
-        padding: '1rem 0', 
-        height: '56px',
+    <div
+      style={{
+        borderBottom: hasScrolled ? '1px solid' : 'none',
         position: 'sticky',
         top: 0, 
         left: 0,
-        backdropFilter: 'saturate(180%) blur(16px)',
-        borderBottom: hasScrolled ? '1px solid' : 'none',
-
-    }}>
-      <Container>
-        <Link href="/">
-          <a>
-            party<span style={{ fontWeight: 700 }}>IRL</span>
-          </a>
-        </Link>
-
-        {breadcrumbs && breadcrumbs.map(({ breadcrumb, href }, idx) => (
-          <span>
-            <span style={{ padding: '0 0.5rem'}}>
-              /
-            </span>
-            <Link href={href} key={`${breadcrumb}-idx`}>
-              <a style={{ fontWeight: idx === breadcrumbs.length - 1 ? 700 : 400}}>
-                {breadcrumb.length > 20 ? `${breadcrumb.slice(0,4)}...${breadcrumb.slice(breadcrumb.length - 4, breadcrumb.length)}`: breadcrumb}
+      }}
+    >
+      <Container 
+        as="nav"
+        css={{ 
+          fontSize: '18px',
+          paddingTop: '1rem', 
+          paddingBottom: '1rem', 
+          height: '56px',
+          backgroundColor: '$background',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+      }}>
+          <div>
+            <Link href="/">
+              <a>
+                party<span style={{ fontWeight: 700 }}>IRL</span>
               </a>
             </Link>
-          </span>
-        ))}
+
+            {breadcrumbs && breadcrumbs.map(({ breadcrumb, href }, idx) => (
+              <span key={`${breadcrumb}-idx`}>
+                {href !== '/' && (
+                  <span style={{ padding: '0 0.5rem'}}>
+                    /
+                  </span>
+                )}
+                <Link href={href}>
+                  <a style={{ fontWeight: idx === breadcrumbs.length - 1 ? 700 : 400}}>
+                    {breadcrumb.length > 20 ? `${breadcrumb.slice(0,4)}...${breadcrumb.slice(breadcrumb.length - 4, breadcrumb.length)}`: breadcrumb}
+                  </a>
+                </Link>
+              </span>
+            ))}
+          </div>
+
+          <div>
+            <button
+              style={{
+                fontSize: '24px',
+                padding: 0,
+                border: 0,
+                backgroundColor: 'transparent',
+                lineHeight: 1,
+                cursor: 'pointer',
+              }}
+              onClick={() => setTheme(isDark ? 'light' : 'dark')}
+            >
+              ◑
+            </button>
+          </div>
       </Container>
-    </nav>
+    </div>
   );
 };
 
