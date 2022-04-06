49812e2ffdbbf53fce502cd1527a475539ea13dd
yihwan <hi@yihwan.kim>
2022-04-06 18:38:09 -0400

    yayaya

--- app/src/components/page/home/index.tsx
+++ app/src/components/page/home/index.tsx
@@ -1,22 +1,48 @@
 import Link from 'next/link';
-import { Text, Spacer } from '@nextui-org/react';
+import { Text, Spacer, Container, Button } from '@nextui-org/react';
 
 const Home = () => {
   return (
     <>
-      <Text h1 css={{ fontWeight: 'unset' }}>
-        party<span style={{ fontWeight: 700 }}>IRL</span> 🎉
+      <Text h1 weight="normal">
+        party<span style={{ fontWeight: 700 }}>IRL</span>
       </Text>
-      <Text h4 css={{ fontWeight: 400 }}>
-        Built on Solana, just for fun.
+      <Text h4 weight="normal">
+        Built on Solana, just for fun. 🎉
       </Text>
 
       <Spacer y={2} />
 
-      <Link href="/join">join</Link>
+      <Container css={{
+        padding: 0,
+        display: 'flex'
+      }}>
+        <Link href="/join">
+          <Button color="gradient" auto>
+            join
+          </Button>
+        </Link>
 
-      <Spacer y={1} />
-      <Link href="/start">start</Link>
+        <Link href="/start">
+          <Button light auto css={{ padding: 0, marginLeft: '1.5rem'}}>
+            start
+          </Button>
+        </Link>
+      </Container>
+
+      <Spacer y={3} />
+
+      <Text h3>How it (should) work:</Text>
+      <Container>
+        <ol>
+          <li>Join or start a party.</li>
+          <li>(If you start a party, you still have to "join" separately because blockchain stuff.)</li>
+          <li>Stake some SOL to join a party.</li>
+          <li>When party time rolls around, check-in.</li>
+          <li>You get your SOL back, plus the SOL from no-shows.</li>
+          <li>Pretty cool, huh?</li>
+        </ol>
+      </Container>
     </>
   );
 };
